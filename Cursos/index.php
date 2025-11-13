3821073011d694978e6268a45af1fcbc12680f3d
Davi Severino <sammueel32@gmail.com>
2025-11-13 10:59:00 -0300

    32

--- Cursos/index.php
+++ Cursos/index.php
@@ -22,10 +22,10 @@
             </div>
         </div>
         <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">SOBRE NÓS</a></li>
-            <li><a href="#">PEDAGÓGICO</a></li>
-            <li class="btn-cursos"><a href="#">CURSOS</a></li>
+            <li><a href="../pagina%20incial/pagina%20incial.html">HOME</a></li>
+            <li><a href="../Sobrenos/index.html">SOBRE NÓS</a></li>
+            <li><a href="../pedagogico/pedagogico.html">PEDAGÓGICO</a></li>
+            <li class="btn-cursos"><a href="../Cursos/index.php">CURSOS</a></li>
         </ul>
     </nav>
 </header>
@@ -34,7 +34,7 @@
     <section class="course-grid">
         <div class="course-card red" data-course-name="Preparatório ENEM">
             <div class="card-content">
-
+                <link href="">
             </div>
         </div>
         <div class="course-card blue" data-course-name="Análises Químicas">
